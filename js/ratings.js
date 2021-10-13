40d4819742b2c9120c28946029468acd0abc5414
Jasmine Ampofo <akubahjazz@gmail.com>
2021-10-13 11:49:03 +0000

    task1-7

---
+++ js/ratings.js
@@ -0,0 +1,18 @@
+(function collect_ratings() {
+    let ratings = {
+        count: 0,
+        sum: 0,
+        average: 0
+    };
+
+    let rating = 0;
+    const elements = document.querySelectorAll(".rating");
+    // console.log(elements);
+
+    elements.forEach(element => {
+        rating = parseInt(element.id.replace('star', ''));
+        ratings.count += parseInt(element.value);
+    });
+    
+})();
+
